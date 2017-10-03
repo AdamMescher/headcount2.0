958798f0847b7519ba29095f856f41ec7015bb58
Adam Mescher <admescher@gmail.com>
2017-10-03 14:56:04 -0600

    Refactor helper.js to clean data in desired format

--- src/helper.js
+++ src/helper.js
@@ -1,15 +1,22 @@
 export default class DistrictRepository {
   constructor( data ){
-    this.data = data.reduce( (acc, elem) => {
+    this.data = data.reduce( (acc, elem, i, arr) => {
       if(!acc[elem.Location]){
-        acc[elem.Location] = {}
+        acc[elem.Location] = {
+          location: elem.Location,
+          data: {},
+        }
       }
 
-      if(!acc[elem.TimeFrame]){
-        acc[elem.Location][elem.TimeFrame] = elem;
+      if(!acc[elem.Location].data[elem.TimeFrame]){
+        acc[elem.Location].data[elem.TimeFrame] =  elem.Data
       }
 
       return acc
     }, {} )
   }
+  findByName(search){
+
+  }
+
 }

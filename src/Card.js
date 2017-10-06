0a57b642e6e5758180432386859a1475106988df
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-10-06 08:47:21 -0600

    add toggle state to Card class

--- src/Card.js
+++ src/Card.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
 
-const Card = (props) => {
-  const mapped = Object.keys(props.yearData).map( (year, index) => {
-    if(props.yearData[year] < 0.5){
-      return (
-        <li className="bad" key={index + Date.now()}>{year}: {props.yearData[year]}</li>
-      )
+class Card extends Component {
+  constructor(){
+    super()
+    this.state = {
+      clicked: false,
     }
+  }
 
-    return (
-      <li className="good" key={index + Date.now()}>{year}: {props.yearData[year]}</li>
+  toggleClick() {
+    console.log('clicked card')
+    this.setState( {
+      clicked: !this.state.clicked,
+    })
+  }
+
+  render() {
+    const mapped = Object.keys(this.props.yearData).map( (year, index) => {
+      if(this.props.yearData[year] < 0.5){
+        return (
+          <li className="bad" key={index + Date.now()}>{year}: {this.props.yearData[year]}</li>
+        )
+      }
+
+      return (
+        <li className="good" key={index + Date.now()}>{year}: {this.props.yearData[year]}</li>
+      )
+    })
+
+    return(
+      <div className="card" onClick={this.toggleClick.bind(this)}>
+        <h2>{this.props.location}</h2>
+        <ul>
+          { mapped }
+        </ul>
+      </div>
     )
-  })
-
-  return(
-    <div className="card">
-      <h2>{props.location}</h2>
-      <ul>
-        { mapped }
-      </ul>
-    </div>
-  )
+  }
 }
 
 Card.propTypes = {
